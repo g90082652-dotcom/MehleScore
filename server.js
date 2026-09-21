84d11c4ae6a7ea199df4d5212e47e0ed2b10fabe
g90082652-dotcom <g90082652@gmail.com>
2026-09-21 16:11:30 +0400

    Update server.js

--- server.js
+++ server.js
@@ -315,65 +315,12 @@ app.post("/api/players", async (req, res) => {
 // =========================
 // HOME
 // =========================
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="az">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-      <title>AliScore</title>
-
-      <style>
-        body {
-          margin: 0;
-          min-height: 100vh;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          background: #07111f;
-          color: white;
-          font-family: Arial, sans-serif;
-        }
-
-        .box {
-          text-align: center;
-          padding: 30px;
-        }
-
-        h1 {
-          font-size: 42px;
-          margin-bottom: 10px;
-        }
-
-        p {
-          color: #9fb0c5;
-          font-size: 18px;
-        }
-
-        .ok {
-          display: inline-block;
-          margin-top: 20px;
-          padding: 12px 20px;
-          border-radius: 12px;
-          background: #123b2a;
-          color: #5cff9d;
-        }
-      </style>
-    </head>
-
-    <body>
-      <div class="box">
-        <h1>⚽ AliScore</h1>
-        <p>PostgreSQL ilə işləyir</p>
-        <div class="ok">SERVER ONLINE</div>
-      </div>
-    </body>
-    </html>
-  `);
+  res.sendFile(__dirname + "/public/index.html");
 });
+
 
 // =========================
 // START
